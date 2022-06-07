93ede543eec34e8b2e72f3f84b9bda9c057ebe3c
rafetdemir <rafettdem.gmail.com>
2022-06-07 22:30:24 +0300

    calisma

--- calisma.py
+++ calisma.py
@@ -22,7 +22,7 @@ class Halk(Human):
 karakterler = []
 
 while True:
-    taraf = input("Karakterin taraf giriniz(Polis, Suclu, Halk)(cıkmak icin q): ")
+    taraf = input("Karakterin tarafını giriniz(Polis, Suclu, Halk)(cıkmak icin q): ")
     if taraf == "q":
         break
     if taraf == "Polis":
@@ -30,22 +30,37 @@ while True:
         yetenek = input("Karakterin yeteneğini giriniz: ")
         unvan = input("Karakterin unvanını giriniz: ")
         P1 = Polis(isim, yetenek, unvan)
-        karakterler.append(P1)
+        karakterler.append(P1) 
+
+        for karakter in karakterler:
+            print(karakter.isim)
+            print(karakter.yetenek)
+            print(karakter.unvan)
+       
     elif taraf == "Suclu":
         isim = input("Karakterin ismini giriniz: ")
         yetenek = input("Karakterin yeteneğini giriniz: ")
         suc = input("Karakterin sucunu giriniz: ")
         S1 = Suclu(isim, yetenek, suc)
         karakterler.append(S1)
+
+        for karakter in karakterler:
+            print(karakter.isim)
+            print(karakter.yetenek)
+            print(karakter.suc)
+
     elif taraf == "Halk":
         isim = input("Karakterin ismini giriniz: ")
         yetenek = input("Karakterin yeteneğini giriniz: ")
         meslek = input("Karakterin meslegini giriniz: ")
         H1 = Halk(isim, yetenek, meslek)
         karakterler.append(H1)
+
+        for karakter in karakterler:
+            print(karakter.isim)
+            print(karakter.yetenek)
+            print(karakter.meslek)
     else:
         print("Lütfen 'Polis, Suclu, Halk' birini seciniz!")
 
-for karakter in karakterler:
-    print(karakter.isim)
 
